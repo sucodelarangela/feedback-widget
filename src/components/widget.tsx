7e469d828854e59c0261b32fb986dfbb45f4a08b
Angela Caldas <aes.caldas@gmail.com>
2022-05-02 19:09:48 -0300

    Install Headless UI and apply Popover function to this first component

--- src/components/widget.tsx
+++ src/components/widget.tsx
@@ -1,27 +1,18 @@
 import { ChatTeardropDots } from 'phosphor-react'
-import { useState } from 'react'
+import { Popover } from '@headlessui/react'
 
 export function Widget() {
-  //const [state value, new state value]
-  const [isWidgetOpen, setIsWidgetOpen] = useState(false)
-
-  function toggleWidgetVisibility() {
-    setIsWidgetOpen(!isWidgetOpen)
-  }
-
   return (
-    <div className='absolute bottom-5 right-5'>
-      {/* {isWidgetOpen ? <p>Hello, world</p> : null} 
-      The line of code above can be shortened like this: */}
-      {isWidgetOpen && <p>Hello, world</p>}
+    <Popover className='absolute bottom-5 right-5'>
+      <Popover.Panel>Hello, world</Popover.Panel>
       
-      <button onClick={toggleWidgetVisibility} className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
+      <Popover.Button className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
         <ChatTeardropDots className="w-6 h-6 "/>
 
         <span className='max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear'>
           <span className='pl-3'></span>
           Feedback</span>
-      </button>
-    </div>
+      </Popover.Button>
+    </Popover>
   )
 }
