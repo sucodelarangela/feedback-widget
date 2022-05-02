32b019ee9ffd95f58a3cdc0590f259903670ae5c
Angela Caldas <aes.caldas@gmail.com>
2022-05-02 15:10:23 -0300

    LEarning and testing components and props

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+interface ButtonProps {
+  text: string;
+  //text?: string; -> o "?" deixa como opcional
+}
+
+function Button(props: ButtonProps){
+  console.log(props.text)
+  return <button>{props.text}</button>
+}
+
 function App() {
-  return <h1>Hello, Angela</h1>
+  return (<div>
+    <h1>Hello, World</h1>
+    <Button text='Enviar'/>
+    <Button text='Cancelar'/>
+    <Button text='12345'/>
+  </div>)
 }
 
 export default App
