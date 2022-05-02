75978f742f487207967ab04f2bf8373f2691aca4
Angela Caldas <aes.caldas@gmail.com>
2022-05-02 18:58:22 -0300

    Learning to use States

--- src/components/widget.tsx
+++ src/components/widget.tsx
@@ -1,5 +1,27 @@
 import { ChatTeardropDots } from 'phosphor-react'
+import { useState } from 'react'
 
 export function Widget() {
-  return <h1><ChatTeardropDots/></h1>
+  //const [state value, new state value]
+  const [isWidgetOpen, setIsWidgetOpen] = useState(false)
+
+  function toggleWidgetVisibility() {
+    setIsWidgetOpen(!isWidgetOpen)
+  }
+
+  return (
+    <div className='absolute bottom-5 right-5'>
+      {/* {isWidgetOpen ? <p>Hello, world</p> : null} 
+      The line of code above can be shortened like this: */}
+      {isWidgetOpen && <p>Hello, world</p>}
+      
+      <button onClick={toggleWidgetVisibility} className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
+        <ChatTeardropDots className="w-6 h-6 "/>
+
+        <span className='max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear'>
+          <span className='pl-3'></span>
+          Feedback</span>
+      </button>
+    </div>
+  )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
 module.exports = {
   content: ['./src/**/*.tsx'], // mudamos de {html,js} para tsx, que é a extensão de arquivo do projeto
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        brand: {
+          500: '#8257e6' // nova variável de cor
+        }
+      }
+    }
   },
   plugins: []
 }
