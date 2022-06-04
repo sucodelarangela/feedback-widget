cfa9c952140fdc3a7005eaa3e668b0729a887d4f
Angela Caldas <aes.caldas@gmail.com>
2022-06-04 14:37:20 -0300

    Creating POST routes for feedbacks using Prisma

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,24 +1,27 @@
-import express from 'express'
-import { prisma } from './prisma'
+import express from 'express';
+import {prisma} from './prisma';
 
-const app = express()
+const app = express();
 
 // Middleware
-app.use(express.json())
+app.use(express.json());
 
-app.post('/feedbacks', (req, res) => {
+app.post('/feedbacks', async (req, res) => {
   // Creating new data inside prisma 'feedback' table
-  prisma.feedback.create({
-    data: {
-      type: req.body.type,
-      comment: req.body.comment,
-      screenshot: req.body.screenshot
-    }
-  })
+  const {type, comment, screenshot} = req.body;
 
-  return res.send("Hello world")
-})
+  const feedback = await prisma.feedback.create({
+    // Using short syntax from destructuring above
+    data: {
+      type, // type: req.body.type
+      comment, // comment: req.body.comment
+      screenshot // screenshot: req.body.screenshot
+    }
+  });
+
+  return res.status(201).json({data: feedback});
+});
 
 app.listen(3333, () => {
-  console.log('HTTP server running')
-})
+  console.log('HTTP server running');
+});
