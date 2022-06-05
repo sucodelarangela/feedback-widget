272a3c75c7eade0966a9c7a122ee67c04aaba8ad
Angela Caldas <aes.caldas@gmail.com>
2022-06-05 15:06:13 -0300

    Config nodemailer and test sending with Mailtrap

--- server/src/server.ts
+++ server/src/server.ts
@@ -7,6 +7,16 @@ const app = express();
 // Middleware
 app.use(express.json());
 
+// Config for nodemailer from www.mailtrap.io
+const transport = nodemailer.createTransport({
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  auth: {
+    user: 'e43d2efac2a37f',
+    pass: '38a628e47f9030'
+  }
+});
+
 app.post('/feedbacks', async (req, res) => {
   // Creating new data inside prisma 'feedback' table
   const {type, comment, screenshot} = req.body;
@@ -18,6 +28,20 @@ app.post('/feedbacks', async (req, res) => {
       comment, // comment: req.body.comment
       screenshot // screenshot: req.body.screenshot
     }
+  });
+
+  // Using mailtrap
+  await transport.sendMail({
+    from: 'Equipe Feedget <oi@feedget.com>',
+    // In dev env, this email will be automatically sent do mailtrap. But on prod env, it's necessary to use a real e-mail below
+    to: 'Angela Caldas <batata@batatamail.com>',
+    subject: 'Novo Feedback',
+    html: [
+      `<div style="font-family: sans-serif; font-size: 16px; color: #111">`,
+      `<p>Tipo do Feedback: ${type}</p>`,
+      `<p>Comentário: ${comment}</p>`,
+      `</div>`
+    ].join('\n')
   });
 
   return res.status(201).json({data: feedback});
