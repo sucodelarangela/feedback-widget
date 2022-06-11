487c05becdcfe853be0c13e33c5864d792a1dfa8
Angela Caldas <aes.caldas@gmail.com>
2022-06-11 15:15:20 -0300

    Created basic structures for Option component

--- mobile/src/components/Copyright/index.tsx
+++ mobile/src/components/Copyright/index.tsx
@@ -1,12 +1,21 @@
 import React from "react";
-import { View } from "react-native";
+import { View, Text } from "react-native";
 
 import { styles } from "./styles";
 
 export function Copyright() {
   return (
-    <View style={styles.container}>
-      {/*  */}
-    </View>
+    <View>
+      <Text style={styles.text}>
+        Feito com ♥ por {''}
+        {/* <a
+        // className="underline underline-offset-2"
+        // href="https://angelacaldas.vercel.app"
+        // target="_blank"
+        > */}
+        Angela Caldas
+        {/* </a> */}
+      </Text>
+    </View >
   )
 }
--- mobile/src/components/Option/styles.ts
+++ mobile/src/components/Option/styles.ts
@@ -2,6 +2,5 @@ import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
   container: {
-    alignItems: "center"
   }
 })
