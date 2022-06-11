98b426cec2de36c43081ed38478da6ba056a6235
Angela Caldas <aes.caldas@gmail.com>
2022-06-11 14:22:56 -0300

    Created feedback button

--- mobile/src/components/Widget/index.tsx
+++ mobile/src/components/Widget/index.tsx
@@ -1,10 +1,22 @@
 // File for the structure of our component
 import React from 'react';
-import { View } from 'react-native';
+import { View, TouchableOpacity } from 'react-native'; // TouchableOpacity = click element
+import { ChatTeardropDots } from 'phosphor-react-native';
 
 import { styles } from './styles';
+import { theme } from '../../theme';
 
 export function Widget() {
   // The style of our component will be the container in styles.ts
-  return <View style={styles.container}></View>;
+  return (
+    <>
+      <TouchableOpacity
+        style={styles.button}>
+        <ChatTeardropDots
+          size={24}
+          weight="bold"
+          color={theme.colors.text_on_brand_color} />
+      </TouchableOpacity>
+    </>
+  )
 }
--- mobile/src/components/Widget/styles.ts
+++ mobile/src/components/Widget/styles.ts
@@ -1,8 +1,18 @@
 // File for the style of our component
 import { StyleSheet } from "react-native";
+import { theme } from "../../theme";
+import { getBottomSpace } from "react-native-iphone-x-helper";
 
 export const styles = StyleSheet.create({
-  container: {
-    // style rules
+  button: {
+    width: 48,
+    height: 48,
+    borderRadius: 24,
+    backgroundColor: theme.colors.brand,
+    justifyContent: 'center',
+    alignItems: "center",
+    position: 'absolute',
+    right: 16,
+    bottom: getBottomSpace() + 16
   }
 })
