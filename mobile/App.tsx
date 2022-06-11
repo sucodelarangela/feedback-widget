58f40439f8cf1a5ca900e5f085a680de7059240c
Angela Caldas <aes.caldas@gmail.com>
2022-06-11 14:55:11 -0300

    Open/close menu functionality with bottomSheet and gesture handler

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -5,7 +5,7 @@ import { View } from 'react-native';
 import AppLoading from 'expo-app-loading';
 import { useFonts, Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
 
-import { Widget } from './src/components/Widget';
+import Widget from './src/components/Widget';
 import { theme } from './src/theme';
 
 export default function App() {
--- mobile/src/components/Widget/index.tsx
+++ mobile/src/components/Widget/index.tsx
@@ -1,22 +1,42 @@
 // File for the structure of our component
-import React from 'react';
+import React, { useRef } from 'react'; // 
 import { View, TouchableOpacity } from 'react-native'; // TouchableOpacity = click element
 import { ChatTeardropDots } from 'phosphor-react-native';
+import BottomSheet from '@gorhom/bottom-sheet';
+import { gestureHandlerRootHOC } from 'react-native-gesture-handler'
 
 import { styles } from './styles';
 import { theme } from '../../theme';
 
-export function Widget() {
-  // The style of our component will be the container in styles.ts
+function Widget() {
+  // anotates bottomSheet reference so we don't need to use states
+  const bottomSheetRef = useRef<BottomSheet>(null)
+
+  // using bottomSheet to open/close menu
+  function handleOpen() {
+    bottomSheetRef.current?.expand()
+    // current? checks if it's null or not
+  }
+
   return (
     <>
       <TouchableOpacity
-        style={styles.button}>
+        style={styles.button}
+        onPress={handleOpen}>
         <ChatTeardropDots
           size={24}
           weight="bold"
           color={theme.colors.text_on_brand_color} />
       </TouchableOpacity>
+
+      <BottomSheet
+        ref={bottomSheetRef}
+        snapPoints={[1, 280]}>
+      </BottomSheet>
     </>
   )
 }
+
+export default gestureHandlerRootHOC(Widget)
+
+/* snapPoints={[closed, open]} */
